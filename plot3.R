e08f89c9d71514a1782947f4024fd1d912f8fd35
Alan Cash <incandescental@gmail.com>
2016-02-04 18:17:44 +0000

    Unit 4- Project 1 v2

--- plot3.R
+++ plot3.R
@@ -17,12 +17,13 @@ pow2$datetime <- strptime(pow2$datetime, "%Y-%m-%d %H:%M:%S")
 
 # Specify graph
 
+png("plot3.png")
+
 plot(pow2$datetime,pow2$Sub_metering_1,type = "l", xlab = "", ylab = "Energy sub metering")
 lines(pow2$datetime, pow2$Sub_metering_2, type = "l", col = "red")
 lines(pow2$datetime, pow2$Sub_metering_3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c("black", "red", "blue"), cex = 0.8) 
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c("black", "red", "blue")) 
 
 # save file
 
-dev.copy(png,'plot3.png')
 dev.off()
